fed29ae9780b2155e2863b939bec38bae30cfbab
Eugene <evd.jeka@gmail.com>
2022-05-31 20:24:07 +0000

    refactor hit system

--- src/ball.rs
+++ src/ball.rs
@@ -1,26 +1,24 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, utils::HashSet};
+use bevy_kira_audio::Audio;
 use bevy_rapier2d::prelude::*;
 
 use crate::{common::GameAssets, PLAYER_BALL_RADIUS};
-
-#[derive(Component)]
-pub struct Ball;
 
 pub struct BallPlugin;
 
 impl Plugin for BallPlugin {
     fn build(&self, app: &mut App) {
-        app.add_system(ball_despawn_system);
+        app.add_system(ball_despawn_system)
+            .add_system(ball_hit_system)
+            .add_system(ball_hit_reaction_system.after(ball_hit_system));
     }
 }
 
-fn ball_despawn_system(mut commands: Commands, balls: Query<(Entity, &Transform), With<Ball>>) {
-    for (entity, tr) in balls.iter() {
-        if tr.translation.x.abs() > 1000.0 || tr.translation.y.abs() > 1000.0 {
-            commands.entity(entity).despawn()
-        }
-    }
-}
+#[derive(Component)]
+pub struct Ball;
+
+#[derive(Component)]
+pub struct HitByBall;
 
 #[derive(Bundle)]
 pub struct BallPhysicsBundle {
@@ -86,4 +84,55 @@ impl BallBundle {
             ball: Ball,
         }
     }
+}
+
+fn ball_despawn_system(mut commands: Commands, balls: Query<(Entity, &Transform), With<Ball>>) {
+    for (entity, tr) in balls.iter() {
+        if tr.translation.x.abs() > 1000.0 || tr.translation.y.abs() > 1000.0 {
+            commands.entity(entity).despawn()
+        }
+    }
+}
+
+pub fn ball_hit_system(
+    mut commands: Commands,
+    rapier_context: Res<RapierContext>,
+    balls: Query<Entity, With<Ball>>,
+    mut last_contact_entities: Local<HashSet<Entity>>,
+) {
+    let mut contact_entities = HashSet::new();
+    balls.for_each(|entity| {
+        for contact_pair in rapier_context.contacts_with(entity) {
+            let other_collider = if contact_pair.collider1() == entity {
+                contact_pair.collider2()
+            } else {
+                contact_pair.collider1()
+            };
+
+            contact_entities.insert(other_collider);
+            if last_contact_entities.contains(&other_collider)
+                || contact_pair
+                    .manifold(0)
+                    .map_or(true, |m| m.num_points() == 0)
+            {
+                continue;
+            }
+            last_contact_entities.insert(other_collider);
+            commands.entity(other_collider).insert(HitByBall);
+        }
+    });
+    last_contact_entities.retain(|e| contact_entities.contains(e));
+}
+
+pub fn ball_hit_reaction_system(
+    mut commands: Commands,
+    game_assets: Res<GameAssets>,
+    audio: Res<Audio>,
+    hit_by_ball: Query<Entity, Added<HitByBall>>,
+) {
+    hit_by_ball.for_each(|entity| {
+        let idx = fastrand::usize(..game_assets.ball_hit_sound.len());
+        audio.play(game_assets.ball_hit_sound[idx].clone());
+        commands.entity(entity).remove::<HitByBall>();
+    })
 }
--- src/peg.rs
+++ src/peg.rs
@@ -5,9 +5,9 @@ use bevy_kira_audio::Audio;
 use bevy_rapier2d::prelude::*;
 
 use crate::{
-    ball::Ball,
+    ball::{ball_hit_reaction_system, ball_hit_system, HitByBall},
     input_state::{GameAction, InputState},
-    GameAssets, GameState, PEG_RADIUS,
+    GameAssets, PEG_RADIUS,
 };
 
 pub struct PegPlugin;
@@ -16,20 +16,19 @@ impl Plugin for PegPlugin {
     fn build(&self, app: &mut App) {
         app.insert_resource(HashSet::<Entity>::new())
             .add_system(spawn_peg_system)
-            .add_system(peg_despawn_system.before(peg_hit_system))
-            .add_system(peg_hit_system)
-            .add_system(peg_hit_reaction_system.after(peg_hit_system));
+            .add_system(peg_despawn_system.before(ball_hit_system))
+            .add_system(peg_hit_system.before(ball_hit_reaction_system));
     }
 }
 
 #[derive(Component)]
-struct Peg;
+pub struct Peg;
 
 #[derive(Component)]
 pub struct PegToDespawn;
 
 #[derive(Component)]
-pub struct PegHit;
+pub struct HitPeg;
 
 fn spawn_peg_system(
     mut commands: Commands,
@@ -66,9 +65,6 @@ fn spawn_peg_system(
     }))
 }
 
-// Note: sometimes rapier reports collision events but doesn't perfom physics calculation.
-// this results in situations where the ball `grazes` the peg causing it to despawn,
-// but doesn't change its own trajectory. Not sure how to fix this.
 fn peg_despawn_system(
     mut commands: Commands,
     mut pegs: Query<Entity, (With<Peg>, With<PegToDespawn>)>,
@@ -79,56 +75,21 @@ fn peg_despawn_system(
 }
 
 fn peg_hit_system(
-    rapier_context: Res<RapierContext>,
     mut commands: Commands,
-    mut game_state: ResMut<GameState>,
-    balls: Query<Entity, With<Ball>>,
-    pegs: Query<Entity, (With<Peg>, Without<PegToDespawn>, Without<PegHit>)>,
-    mut currently_in_contact: Local<HashSet<Entity>>,
-    audio: Res<Audio>,
-    assets: Res<GameAssets>,
-) {
-    let mut contact_entities = HashSet::new();
-    balls.for_each(|entity| {
-        for contact_pair in rapier_context.contacts_with(entity) {
-            let other_collider = if contact_pair.collider1() == entity {
-                contact_pair.collider2()
-            } else {
-                contact_pair.collider1()
-            };
-            contact_entities.insert(other_collider);
-            if currently_in_contact.contains(&other_collider) {
-                continue;
-            }
-            if contact_pair
-                .manifold(0)
-                .map_or(true, |m| m.num_points() == 0)
-            {
-                continue;
-            }
-            if !pegs.contains(other_collider) {
-                currently_in_contact.insert(other_collider);
-                let idx = fastrand::usize(..assets.ball_hit_sound.len());
-                audio.play(assets.ball_hit_sound[idx].clone());
-                continue;
-            }
-            currently_in_contact.insert(other_collider);
-            game_state.player_score += 1;
-            // commands.entity(other_collider).insert(PegToDespawn);
-            commands.entity(other_collider).insert(PegHit);
-            let idx = fastrand::usize(..assets.peg_hit_sound.len());
-            audio.play(assets.peg_hit_sound[idx].clone());
-        }
-    });
-    currently_in_contact.retain(|e| contact_entities.contains(e));
-}
-
-fn peg_hit_reaction_system(
     game_assets: Res<GameAssets>,
-    mut hit_pegs: Query<(&mut Handle<Image>, &mut Sprite), (With<Peg>, Added<PegHit>)>,
+    audio: Res<Audio>,
+    mut hit_pegs: Query<
+        (Entity, &mut Handle<Image>, &mut Sprite),
+        (With<Peg>, Without<HitPeg>, Added<HitByBall>),
+    >,
 ) {
-    for (mut peg_image, mut peg_sprite) in hit_pegs.iter_mut() {
+    for (entity, mut peg_image, mut peg_sprite) in hit_pegs.iter_mut() {
+        let idx = fastrand::usize(..game_assets.peg_hit_sound.len());
+        audio.play(game_assets.peg_hit_sound[idx].clone());
+
         *peg_image = game_assets.peg_hit_image.clone();
         peg_sprite.color = Color::GREEN;
+
+        commands.entity(entity).remove::<HitByBall>().insert(HitPeg);
     }
 }
