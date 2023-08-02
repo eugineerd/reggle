020547a637f151d5c0222b264e843b474ac81d7e
Eugene <evd.jeka@gmail.com>
2023-08-02 15:31:20 +0000

    cursor position now determines  target to hit

--- src/launcher.rs
+++ src/launcher.rs
@@ -1,5 +1,5 @@
 use bevy::prelude::*;
-use bevy_rapier2d::prelude::Velocity;
+use bevy_rapier2d::prelude::{RapierConfiguration, Velocity};
 
 use crate::common::{GameState, InGameState};
 use crate::LAUNCHER_BASE_POWER;
@@ -26,7 +26,6 @@ impl Plugin for LauncherPlugin {
 }
 
 #[derive(Component, Reflect, Default)]
-#[reflect(Component, Default)]
 pub struct Launcher {
     pub direction: Vec2,
     pub power: f32,
@@ -56,16 +55,36 @@ fn setup_ball_launcher(mut commands: Commands, game_assets: Res<GameAssets>) {
     ));
 }
 
+fn angle_to_hit_target(start_pos: Vec2, target_pos: Vec2, g: f32, v: f32) -> f32 {
+    let target = target_pos - start_pos;
+    let (x, y) = (target.x, target.y);
+    let r = (v.powi(4) - g * (g * x * x + 2.0 * y * v * v)).sqrt();
+    let mut r = ((v * v - r) / (g * x)).atan();
+    if x < 0.0 {
+        r = r - std::f32::consts::PI;
+    }
+    r
+}
+
 fn launcher_control_system(
     input_state: Res<GameInput>,
+    rapier_config: Res<RapierConfiguration>,
     mut launcher: Query<(&mut Transform, &mut Launcher)>,
 ) {
     let (mut tr, mut launcher) = launcher.single_mut();
     if input_state.active(GameAction::MoveLauncher) {
         tr.translation = input_state.cursor_position.extend(0.0);
     }
-    let diff = input_state.cursor_position - tr.translation.truncate();
-    let direction = diff.normalize_or_zero();
+    let target_angle = angle_to_hit_target(
+        tr.translation.truncate(),
+        input_state.cursor_position,
+        rapier_config.gravity.length(),
+        launcher.power,
+    );
+    if target_angle.is_nan() {
+        return;
+    }
+    let direction = Vec2::from_angle(target_angle);
     launcher.direction = direction;
     tr.rotation = Quat::from_rotation_arc_2d(Vec2::new(0.0, 1.0), direction);
 }
